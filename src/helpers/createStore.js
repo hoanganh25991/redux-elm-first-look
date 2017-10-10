266c829e4cddd153ae75708dd6f31a101f68bdd6
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-10-10 10:47:43 +0700

    Compose to DevTools to see dispatch action & state

--- src/helpers/createStore.js
+++ src/helpers/createStore.js
@@ -1,9 +1,10 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import {install} from '@jarvisaoieong/redux-loop';
 import createLogger from '@jarvisaoieong/redux-logger';
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly"
 
 export default (reducer, initialState) =>
   createStore(reducer, initialState, compose(
     install(),
-    applyMiddleware(createLogger({collapsed: true})),
+    composeWithDevTools(applyMiddleware(createLogger({collapsed: true}))),
   ))
